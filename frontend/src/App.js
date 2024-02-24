ddc86043c3040b8889d1dba3f00f2b82e6d20f26
mucoban <webdeveloper.mucahitcoban@gmail.com>
2024-02-25 01:32:44 +0300

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="search-sec">
+            <input className="search-itext" type="text" placeholder="Search Term" />
+            <button className="i-btn">Search</button>
+            <div className="stats">
+                <div className="stats-txt">
+                    <span className="oc-index">1</span>
+                    <span className="oc-count">/ 20</span>
+                    <span className="main">occurences found</span>
+                </div>
+            </div>
+        </div>
+        <div className="replace-sec">
+            <input className="search-itext" type="text" placeholder="Replace Term" />
+            <button className="i-btn">Replace</button>
+            <div className="stats">
+                <div className="stats-txt">
+                    <span className="oc-index">11</span>
+                    <span className="oc-count"></span>
+                    <span className="main">occorunces replaced</span>
+                </div>
+            </div>
+        </div>
+        <div className="text-action-buttons">
+            <button className="tab-btn">Reset Text</button>
+            <button className="tab-btn">Copy Text</button>
+        </div>
+      </div>
+      <div className="main-text-section">
+        <textarea className="ta" placeholder="paste your text here"></textarea>
+      </div>
+    </div>
+  );
+}
+
+export default App;
